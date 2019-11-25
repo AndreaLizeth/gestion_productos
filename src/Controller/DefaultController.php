d83573a458039f9d6ac292598b6312c7803a84e1
AndreaLizeth <andrea.quishpe01@epn.edu.ec>
2019-11-25 13:36:05 -0500

    Inicializacion Bootstrap

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,15 +2,11 @@
 
 namespace App\Controller;
 
-
-
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route("/categoria")
- */
+
 class DefaultController extends AbstractController
 {
     /**
