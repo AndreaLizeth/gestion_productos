871fc5caf07432ba2b59015e1b11de827e3b1010
AndreaLizeth <andrea.quishpe01@epn.edu.ec>
2019-11-30 23:28:14 -0500

    arreglar

--- src/Controller/CountryController.php
+++ src/Controller/CountryController.php
@@ -1,29 +1,16 @@
 <?php
-
 namespace App\Controller;
-
-<<<<<<< HEAD
 use App\Entity\City;
-=======
->>>>>>> 1be157315573d9ef9a056ec3cff3e5cb2875fc19
 use App\Entity\Country;
 use App\Form\CountryType;
 use App\Repository\CountryRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-<<<<<<< HEAD
 use Symfony\Component\HttpFoundation\JsonResponse;
-=======
->>>>>>> 1be157315573d9ef9a056ec3cff3e5cb2875fc19
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-
 /**
-<<<<<<< HEAD
  * @Route("/pais")
-=======
- * @Route("/country")
->>>>>>> 1be157315573d9ef9a056ec3cff3e5cb2875fc19
  */
 class CountryController extends AbstractController
 {
@@ -36,34 +23,25 @@ class CountryController extends AbstractController
             'countries' => $countryRepository->findAll(),
         ]);
     }
-
     /**
-<<<<<<< HEAD
      * @Route("/nuevo", name="country_new", methods={"GET","POST"})
-=======
-     * @Route("/new", name="country_new", methods={"GET","POST"})
->>>>>>> 1be157315573d9ef9a056ec3cff3e5cb2875fc19
      */
     public function new(Request $request): Response
     {
         $country = new Country();
         $form = $this->createForm(CountryType::class, $country);
         $form->handleRequest($request);
-
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($country);
             $entityManager->flush();
-
             return $this->redirectToRoute('country_index');
         }
-
         return $this->render('country/new.html.twig', [
             'country' => $country,
             'form' => $form->createView(),
         ]);
     }
-
     /**
      * @Route("/{id}", name="country_show", methods={"GET"})
      */
@@ -73,31 +51,22 @@ class CountryController extends AbstractController
             'country' => $country,
         ]);
     }
-
     /**
-<<<<<<< HEAD
      * @Route("/{id}/editar", name="country_edit", methods={"GET","POST"})
-=======
-     * @Route("/{id}/edit", name="country_edit", methods={"GET","POST"})
->>>>>>> 1be157315573d9ef9a056ec3cff3e5cb2875fc19
      */
     public function edit(Request $request, Country $country): Response
     {
         $form = $this->createForm(CountryType::class, $country);
         $form->handleRequest($request);
-
         if ($form->isSubmitted() && $form->isValid()) {
             $this->getDoctrine()->getManager()->flush();
-
             return $this->redirectToRoute('country_index');
         }
-
         return $this->render('country/edit.html.twig', [
             'country' => $country,
             'form' => $form->createView(),
         ]);
     }
-
     /**
      * @Route("/{id}", name="country_delete", methods={"DELETE"})
      */
@@ -108,10 +77,8 @@ class CountryController extends AbstractController
             $entityManager->remove($country);
             $entityManager->flush();
         }
-
         return $this->redirectToRoute('country_index');
     }
-<<<<<<< HEAD
     /**
      * @Route("/ciudad_pais", name="cities_by_country", condition="request.headers.get('X-Requested-With') == 'XMLHttpRequest'")
      */
@@ -122,6 +89,4 @@ class CountryController extends AbstractController
         $cities = $em->getRepository(City::class)->findByCountry($country_id);
         return new JsonResponse($cities);
     }
-=======
->>>>>>> 1be157315573d9ef9a056ec3cff3e5cb2875fc19
 }
