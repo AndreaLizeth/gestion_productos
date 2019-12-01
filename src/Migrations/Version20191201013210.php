682408b5f5ec4fe63eb26522f2c0b763c1ffe96f
AndreaLizeth <andrea.quishpe01@epn.edu.ec>
2019-11-30 23:10:00 -0500

    Select dependientes*Pais Ciudad- Registro usuario

--- src/Migrations/Version20191201013210.php
+++ src/Migrations/Version20191201013210.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20191201001216 extends AbstractMigration
+final class Version20191201013210 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -22,9 +22,9 @@ final class Version20191201001216 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('CREATE TABLE city (id INT AUTO_INCREMENT NOT NULL, country_id INT NOT NULL, name VARCHAR(150) NOT NULL, INDEX IDX_2D5B0234F92F3E70 (country_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('CREATE TABLE country (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(150) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
-        $this->addSql('ALTER TABLE city ADD CONSTRAINT FK_2D5B0234F92F3E70 FOREIGN KEY (country_id) REFERENCES country (id)');
+        $this->addSql('ALTER TABLE fos_user ADD city_id INT NOT NULL');
+        $this->addSql('ALTER TABLE fos_user ADD CONSTRAINT FK_957A64798BAC62AF FOREIGN KEY (city_id) REFERENCES city (id)');
+        $this->addSql('CREATE INDEX IDX_957A64798BAC62AF ON fos_user (city_id)');
     }
 
     public function down(Schema $schema) : void
@@ -32,8 +32,8 @@ final class Version20191201001216 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE city DROP FOREIGN KEY FK_2D5B0234F92F3E70');
-        $this->addSql('DROP TABLE city');
-        $this->addSql('DROP TABLE country');
+        $this->addSql('ALTER TABLE fos_user DROP FOREIGN KEY FK_957A64798BAC62AF');
+        $this->addSql('DROP INDEX IDX_957A64798BAC62AF ON fos_user');
+        $this->addSql('ALTER TABLE fos_user DROP city_id');
     }
 }
--- src/Repository/CityRepository.php
+++ src/Repository/CityRepository.php
@@ -36,6 +36,15 @@ class CityRepository extends ServiceEntityRepository
     }
     */
 
+    public function findByCountry($country_id)
+    {
+        return $this->createQueryBuilder('city')
+            ->andWhere('city.country = :country')
+            ->setParameter('country', $country_id)
+            ->orderBy('city.name', 'ASC')
+            ->getQuery()
+            ->getArrayResult();
+    }
     /*
     public function findOneBySomeField($value): ?City
     {
@@ -47,4 +56,5 @@ class CityRepository extends ServiceEntityRepository
         ;
     }
     */
+
 }
